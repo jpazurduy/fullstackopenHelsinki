d25e32e9d684740feb7c8a005e6345d066f45925
Jorge Azurduy <jpa.azurduy@gmail.com>
2025-04-20 21:43:15 -0400

    1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,47 @@
+
+import React, { useState } from 'react'
+
+const Buttons = () => {
+  return (
+    <div>
+      <button>good</button>
+      <button>neutral</button>
+      <button>bad</button>
+    </div>
+  )
+}
+
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <p>good    {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad     {props.bad}</p>
+    </div>
+  )
+}
+
+
+
+
+function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>give feedback</h1>
+        <button onClick={() => setGood(good +1 )}>good</button>
+        <button onClick={() => setNeutral(neutral +1 )}>neutral</button>
+        <button onClick={() => setBad(bad +1 )}>bad</button>
+        <h1>statistics</h1>
+        <Statistics good={good} neutral={neutral} bad={bad}/>
+      </div>
+    </>
+  )
+}
+
+export default App
