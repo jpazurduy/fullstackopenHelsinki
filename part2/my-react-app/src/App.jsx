96fa7e8b4c327d6dd5d1c41166f2380b519a3704
Jorge Azurduy <jpa.azurduy@gmail.com>
2025-04-24 19:41:24 -0400

    2.1

---
+++ part2/my-react-app/src/App.jsx
@@ -0,0 +1,81 @@
+
+import React from 'react'
+
+const Part = (props) => {
+  return (
+    <div>
+       <p>{props.part} {props.exercise}</p>
+    </div>
+  )
+}
+
+
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props.parts[0].name)
+  return (
+    <div>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[0].exercises + props.parts[0].exercises}</p>
+    </div>
+  )
+}
+
+
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course.name}/>
+      <Content parts={props.course.parts}/>
+      <Total parts={props.course.parts}/>
+    </div>
+  )
+}
+
+
+
+function App() {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
---
+++ part2/my-react-app/src/main.jsx
@@ -0,0 +1,9 @@
+import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
