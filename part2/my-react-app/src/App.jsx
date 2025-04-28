d3796c8b84b6e6ef5c8920d7a3961733ea183aa9
Jorge Azurduy <jpa.azurduy@gmail.com>
2025-04-27 22:46:54 -0400

    2.5

--- part2/my-react-app/src/App.jsx
+++ part2/my-react-app/src/App.jsx
@@ -1,61 +1,7 @@
 
 import React from 'react'
+import Course from './Course'
 
-const Part = (props) => {
-  return (
-    <div>
-       <p>{props.part} {props.exercise}</p>
-    </div>
-  )
-}
-
-
-const Header = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  console.log(props.parts)
-  console.log("====================")
-  return (
-    <div>
-      {props.parts.map(part => (
-        <Part part={part.name} exercise={part.exercises} />
-      ))}
-    </div>
-  )
-}
-
-const Total = (props) => {
-  console.log(props.parts)
-  
-  const sum = props.parts.reduce((sum, part) => sum + part.exercises, 0 )
-  return (
-    <div>
-      <p>Number of exercises {sum}</p>
-    </div>
-  )
-}
-
-const Course = ({courses}) => {
-  
-  return (
-    <div>
-      { courses.map( course => (
-        <div key={course.id}>
-          <Header course={course.name}/>
-          <Content parts={course.parts}/>
-          <Total parts={course.parts}/>
-        </div>
-      ))}
-    </div>
-  )
-}
 
 
 function App() {
