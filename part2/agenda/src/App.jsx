c4c3c61134acff5da045bf71ac5ece77add123a4
Jorge Azurduy <jpa.azurduy@gmail.com>
2025-06-01 23:06:10 -0400

    2.9

--- part2/agenda/src/App.jsx
+++ part2/agenda/src/App.jsx
@@ -3,10 +3,21 @@ import { useState } from 'react'
 
 
 function App() {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-123456' }]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
 
+  const [searchTerm, setSearchTerm] = useState("")
+  
+  const showFilteredContacts = persons.filter(person =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+  
   const addingNewName = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
@@ -16,6 +27,11 @@ function App() {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
+
+  const addingSearchTerm = (event) => {
+    console.log(event.target.value)
+    setSearchTerm(event.target.value)
+  }
   
   const addData = (event) => {
     event.preventDefault()
@@ -23,7 +39,7 @@ function App() {
       alert(`${newName} is already added to phonebook`)
       return
     }
-    setPersons(persons.concat({name: newName, number: newNumber}))
+    setPersons(persons.concat({name: newName, number: newNumber, id: persons.length + 1}))
     alert(`${newName} added to phonebook`)
     
     setNewName("")
@@ -32,6 +48,11 @@ function App() {
 
   return (
     <div>
+    <div>
+        <h3>Phonebook Search</h3>
+        Search : <input value={searchTerm} onChange={addingSearchTerm}/>
+    </div>
+    
       <h2>Phonebook</h2>
       <form onSubmit={addData}>
         <div>
@@ -46,8 +67,9 @@ function App() {
         </div>
       </form>
       <h2>Numbers</h2>
+      
       <ul>
-        {persons.map((person, index) => (
+        {showFilteredContacts.map((person, index) => (
           <li key={index}>{person.name} {person.number}</li>
         ))}
       </ul>
